d45af6e35781d5e3a0a1105122d3296702a0cc99
peter <peteranderssonkolback@gmail.com>
2018-09-06 10:32:16 +0200

    added Lab1

---
+++ Lab1Ass2/Lab1Ass2/Debug/makedep.mk
@@ -0,0 +1,20 @@
+################################################################################
+# Automatically-generated file. Do not edit or delete the file
+################################################################################
+
+src\ASF\avr32\boards\evk1100\init.c
+
+src\ASF\avr32\boards\evk1100\led.c
+
+src\ASF\avr32\drivers\gpio\gpio.c
+
+src\ASF\avr32\drivers\intc\exception.S
+
+src\ASF\avr32\drivers\intc\intc.c
+
+src\ASF\avr32\utils\startup\startup_uc3.S
+
+src\ASF\avr32\utils\startup\trampoline_uc3.S
+
+src\main.c
+
